e8ae53df1dfe213550aa8bf8563168e287873313
Trimitor <p.trimitor@gmail.com>
2023-11-11 18:57:05 +0300

    Update MPQ.js

--- tools/js/MPQ.js
+++ tools/js/MPQ.js
@@ -1,6 +1,6 @@
 // Blizzard MPQ Archive on JS
 
-const locales = {
+const MPQLocales = {
     def: 0, // neutral
     zhCN: 0x404, // Chinese
     csCZ: 0x405, // Czech
@@ -18,20 +18,33 @@ const locales = {
     enUK: 0x809 // English (UK)
 }
 
+const MPQ_FILE_IMPLODE 			    = 0x00000100;
+const MPQ_FILE_COMPRESS 		    = 0x00000200;
+const MPQ_FILE_ENCRYPTED 		    = 0x00010000;
+const MPQ_FILE_FIX_KEY			    = 0x00020000;
+const MPQ_FILE_SINGLE_UNIT		    = 0x01000000;
+const MPQ_FILE_DELETE_MARKER        = 0x02000000;
+const MPQ_FILE_SECTOR_CRC	 	    = 0x04000000;
+const MPQ_FILE_EXISTS		 	    = 0x80000000;
+
 class MPQ {
-    constructor() {
+    constructor(files, listfile=true, lang="def") {
+        this.files = files
+        this.listfile = listfile
+        this.lang = MPQLocales[lang]
+        if (this.files === undefined) {
+            throw new Error('files is required');
+        }
+        if (typeof this.files === 'string') {
+            this.files = [this.files];
+        }
+    }
+
+    create() {
         
     }
 
-    createArchive = async (fileslist, language = locales.def) => {
-        switch (fileslist.type) {
-            case 'string':
-                return await fetch(fileslist.path).then(res => res.arrayBuffer());
-            case 'object':
-
-                break;
-            default:
-                throw new Error(`Invalid type: ${data.type}`);
-        }
+    read() {
+        
     }
 }
