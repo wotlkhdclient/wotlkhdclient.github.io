53ca7e7c9f9d2086f1d35700c9bceb59409226fc
Trimitor <p.trimitor@gmail.com>
2024-02-12 21:49:43 +0300

    upd

--- tools/js/DBCUtil.js
+++ tools/js/DBCUtil.js
@@ -1,300 +1,262 @@
+/**
+ * DBC utility implementation in JavaScript.
+ * @author: Trimitor
+ * @license: MIT
+ * @version: 1.0
+ * Wiki: https://wowdev.wiki/DBC
+ */
+
+'use strict';
+
 const MAGIC_NUMBER = 1128416343; // WDBC
-const debug = false; // debug mode
-
-loadFrom = async (file) => {
-    if (typeof file === 'string') {
-        return await fetch(file).then(res => res.arrayBuffer());
-    }
-    if (file instanceof ArrayBuffer) {
-        return file;
-    }
-    if (file instanceof Blob) {
-        return new Promise((resolve, reject) => {
-            const fileReader = new FileReader();
-            fileReader.onload = () => resolve(fileReader.result);
-            fileReader.onerror = () => reject(fileReader.error);
-            fileReader.readAsArrayBuffer(file);
-        })
-    }
-    throw new Error(`Invalid file: ${file}`);
+const typeSyzes = { // in bytes
+    'long': 4,
+    'str': 4,
+    'float': 4,
+    'flags': 4,
+    'bool' : 1,
 }
 
-createFrom = async (file, delimiter = ',') => {
-    if (typeof file === 'string') {
-        const text = await (await fetch(file)).text();
-        const rows = text.split(/\r\n/g);
-        const columnHeaders = rows.shift().split(delimiter);
-        return rows.map(row => {
-            const columns = row.split(delimiter);
-            const obj = {};
-            columnHeaders.forEach((header, i) => {
-                obj[header] = columns[i];
-            });
-            return obj;
-        })
+var debug = false;
 
+class DBC
+{
+    constructor(data, schema)
+    {
+        if (!data || !data.length) throw new Error("Data is required");
+        if (!schema || !schema.length) throw new Error("Schema is required");
+        
+        this.data = data;
+        this.schema = schema;
     }
-
-    if (file instanceof Blob) {
-        const text = await new Promise((resolve, reject) => {
-            const fileReader = new FileReader();
-            fileReader.onload = () => resolve(fileReader.result);
-            fileReader.onerror = () => reject(fileReader.error);
-            fileReader.readAsText(file);
-        });
-
-        const rows = text.split(/\r\n/g);
-        const columnHeaders = rows.shift().split(delimiter);
-        return rows.map(row => {
-            const columns = row.split(delimiter);
-            const obj = {};
-            columnHeaders.forEach((header, i) => {
-                obj[header] = columns[i];
-            });
-            return obj;
-        });
+    async loadSchema() {
+        return await fetch(this.schema).then(response => response.json());
     }
-
-    if (file instanceof Array) {
-        return file;
-    }
-
-    throw new Error(`Invalid file: ${typeof file}`);
-}
-
-toCSV = (obj) => {
-    const lineBreak = '\r\n';
-    const delimiter = ',';
-
-    const header = Object.keys(obj[0]);
-    const rows = obj.map(row => header.map(fieldName => row[fieldName]).join(delimiter));
-
-    return [header.join(delimiter), ...rows].join(lineBreak);
-}
-
-class DBC {
-    constructor(path, schemaPath) {
-        if (!path) throw new Error('path is required');
-        if (!schemaPath) throw new Error('schemaPath is required');
-        this.path = path;
-        this.schemaPath = schemaPath;
-    }
-
-    getSchema = async () => {
-        const schema = await fetch(`${this.schemaPath}`).then(res => res.json());
-        if (debug) console.log(schema);
-        return schema;
-    }
-
-    getRecordSize = (schema) => {
-        const typeSizes = {
-            'long': 4,
-            'str': 4,
-            'float': 4,
-            'flags': 4,
-            'bool': 1
-        };
-        return schema.fields.reduce((size, field) => {
-            if (!typeSizes[field.type]) {
-                throw new Error(`Invalid field type: ${field.type}`);
-            }
-            return size + typeSizes[field.type];
+    getRecordSize(schema) {
+        return schema.fields.reduce((acc, field) => {
+            if (!field.type) throw new Error('Unknown field type: ' + field.type);
+            return acc + typeSyzes[field.type];
         }, 0);
     }
-
-    read = async () => {
-        const schema = await this.getSchema();
-
+    async read(options = {type : 'csv', delimiter : ',', lineBreak : '\r\n'})
+    {
+        var schema = await this.loadSchema(); // load schema first
+        var offset = 0; // offset in bytes
+        
+        // header data template
         this.magic = 0;
         this.record_count = 0;
         this.field_count = 0;
         this.record_size = 0;
         this.string_block_size = 0;
 
-        return loadFrom(this.path).then(buffer => {
-            if (debug) console.log(buffer);
-            const view = new DataView(buffer);
-            let offset = 0;
+        return loadFrom(this.data).then(buffer => {
+            var reader = new DataView(buffer); // create reader
 
-            this.magic = view.getUint32(offset, true);
+            this.magic = reader.getUint32(offset, true);
+            if (this.magic !== MAGIC_NUMBER) throw new Error('Invalid magic number: ' + this.magic);
             offset += 4;
-            if (this.magic !== MAGIC_NUMBER) throw new Error(`Invalid magic number: ${this.magic}`);
-            this.record_count = view.getUint32(offset, true);
+            this.record_count = reader.getUint32(offset, true);
             offset += 4;
-            this.field_count = view.getUint32(offset, true);
+            this.field_count = reader.getUint32(offset, true);
             offset += 4;
-            this.record_size = view.getUint32(offset, true);
+            this.record_size = reader.getUint32(offset, true);
             offset += 4;
-            this.string_block_size = view.getUint32(offset, true);
+            this.string_block_size = reader.getUint32(offset, true);
             offset += 4;
 
-            this.rows = [];
+            this.rows = []; // rows
 
-            for (let i = 0; i < this.record_count; i++) {
-                const row = {};
-                for (let j = 0; j < this.field_count; j++) {
-                    const field = schema.fields[j];
-                    row[field.name] = '';
+            for (var i = 0; i < this.record_count; i++) {
+                var row = {};
+                for (var j = 0; j < this.field_count; j++) {
+                    var field = schema.fields[j];
+                    row[field.name] = ''; // value for string fields
                     switch (field.type) {
-                        case 'str':
-                            let stroffest = view.getUint32(offset, true);
-                            for (let k = buffer.byteLength - this.string_block_size + stroffest; k < buffer.byteLength; k++) {
-                                if (view.getUint8(k, true) === 0) break;
-                                row[field.name] += String.fromCharCode(view.getUint8(k, true));
-                            }
-                            offset += 4;
-                            break;
                         case 'long':
-                            row[field.name] = view.getInt32(offset, true);
-                            offset += 4;
-                            break;
-                        case 'flags':
-                            row[field.name] = `0x${Number(view.getUint32(offset, true)).toString(16)}`;
+                            row[field.name] = reader.getInt32(offset, true);
                             offset += 4;
                             break;
                         case 'float':
-                            row[field.name] = view.getFloat32(offset, true);
+                            row[field.name] = reader.getFloat32(offset, true); 
+                            offset += 4;
+                            break;
+                        case 'flags':
+                            row[field.name] = '0x' + Number(reader.getUint32(offset, true)).toString(16); // convert to hex
+                            offset += 4;
+                            break;
+                        case 'str':
+                            var string_offset = reader.getUint32(offset, true);
+                            for (var k = buffer.byteLength - this.string_block_size + string_offset; k < buffer.byteLength; k++) {
+                                var character = reader.getUint8(k, true);
+                                if (character === 0) break;
+                                row[field.name] += String.fromCharCode(character);
+                            }
                             offset += 4;
                             break;
                         case 'bool':
-                            row[field.name] = view.getUint8(offset);
+                            row[field.name] = reader.getUint8(offset, true);
                             offset += 1;
                             break;
                         default:
-                            throw new Error(`Invalid field type: ${field.type}`);
+                            throw new Error('Unknown field type: ' + field.type);
                     }
                 }
                 this.rows.push(row);
             }
 
-            if (debug) {
-                console.log(
-                    `=============== Read Debug ===============`
-                );
-                console.log(
-                    `Magic: ${this.magic}\n` +
-                    `Record count: ${this.record_count}\n` +
-                    `Field count: ${this.field_count}\n` +
-                    `Record size: ${this.record_size}\n` +
-                    `String block size: ${this.string_block_size}\n`
-                );
-                console.log(this.rows)
+            switch (options.type) {
+                case 'csv':
+                    const header = Object.keys(this.rows[0]);
+                    const rows = this.rows.map(row => header.map(fieldName => row[fieldName]).join(options.delimiter));
+                    return [header.join(options.delimiter), ...rows].join(options.lineBreak);
+                case 'json':
+                    return JSON.stringify(this.rows);
+                case 'array':
+                    return this.rows;
+                default:
+                    throw new Error('Unknown type: ' + options.type);
             }
-
-            return this.rows;
-
-
         })
     }
 
-    toCSV = async () => {
-        const res = await this.read();
-        return toCSV(res);
-    }
+    async write(options = {delimiter : ',', lineBreak : '\r\n', header : true})
+    {
+        var schema = await this.loadSchema(); // load schema first
+        var offset = 0; // offset in buffer
 
-    write = async () => {
-        const schema = await this.getSchema();
-
+        // header data template
         this.magic = 0;
         this.record_count = 0;
         this.field_count = 0;
         this.record_size = 0;
-        this.string_block_size = 0;
+        this.string_block_size = 0; // empty string block size
 
-        this.string_block = [];
+        this.string_block = new Map();
 
-        return createFrom(this.path).then(array => {
+        return createFrom(this.data, options).then(array => {
+            var string_block_offset = 0;
+            this.rows = array;
+
             this.magic = MAGIC_NUMBER;
-            this.record_count = array.length;
+            this.record_count = this.rows.length;
             this.field_count = schema.fields.length;
             this.record_size = this.getRecordSize(schema);
 
-            this.string_block[0] = '';
-            this.string_block_size++;
+            this.string_block.set('', string_block_offset);
+            string_block_offset += 1;
 
-            for (let row of array) {
-                for ( let field of schema.fields ) {
-                    if (field.type === 'str' && !this.string_block.includes(row[field.name])) {
-                        this.string_block[this.string_block_size] = row[field.name];
-                        this.string_block_size += row[field.name].length + 1;
+            for (var row of this.rows) {
+                for (var field of schema.fields) {
+                    if (field.type === 'str' && !this.string_block.has(row[field.name])) {
+                        this.string_block.set(row[field.name], string_block_offset);
+                        string_block_offset += row[field.name].length + 1;
                     }
                 }
             }
 
-            const buffer = new ArrayBuffer(20 + this.record_count * this.record_size + this.string_block_size);
-            const view = new DataView(buffer);
-            let offset = 0;
-            view.setUint32(offset, this.magic, true);
+            this.string_block_size = string_block_offset;
+
+            var buffer = new ArrayBuffer(20 + this.record_count * this.record_size + this.string_block_size); // create buffer
+            var writer = new DataView(buffer); // create writer
+            
+            // write header data
+            writer.setUint32(offset, this.magic, true);
             offset += 4;
-            view.setUint32(offset, this.record_count, true);
+            writer.setUint32(offset, this.record_count, true);
             offset += 4;
-            view.setUint32(offset, this.field_count, true);
+            writer.setUint32(offset, this.field_count, true);
             offset += 4;
-            view.setUint32(offset, this.record_size, true);
+            writer.setUint32(offset, this.record_size, true);
             offset += 4;
-            view.setUint32(offset, this.string_block_size, true);
+            writer.setUint32(offset, this.string_block_size, true);
             offset += 4;
 
-            for (let row of array) {
-                for ( let field of schema.fields ) {
-                    const value = row[field.name];
+            for (var row of this.rows) {
+                for (var field of schema.fields) {
                     switch (field.type) {
-                        case 'str':
-                            view.setUint32(offset, this.string_block.indexOf(value), true);
-                            offset += 4;
-                            break;
                         case 'long':
-                            view.setInt32(offset, value, true);
-                            offset += 4;
-                            break;
-                        case 'flags':
-                            view.setUint32(offset, parseInt(value, 16), true);
+                            writer.setInt32(offset, row[field.name], true);
                             offset += 4;
                             break;
                         case 'float':
-                            view.setFloat32(offset, value, true);
+                            writer.setFloat32(offset, row[field.name], true);
+                            offset += 4;
+                            break;
+                        case 'flags':
+                            writer.setUint32(offset, parseInt(row[field.name], 16), true);
+                            offset += 4;
+                            break;
+                        case 'str':
+                            writer.setUint32(offset, this.string_block.get(row[field.name]), true);
                             offset += 4;
                             break;
                         case 'bool':
-                            view.setUint8(offset, value, true);
+                            writer.setUint8(offset, row[field.name], true);
                             offset += 1;
                             break;
                         default:
-                            throw new Error(`Invalid field type: ${field.type}`);
+                            throw new Error('Unknown field type: ' + field.type);
                     }
                 }
             }
 
-            this.string_block.forEach(str => {
-                for (let i = 0; i < str.length; i++) {
-                    view.setUint8(offset, str.charCodeAt(i));
+
+            for (var str of this.string_block.keys()) {
+                for (var i = 0; i < str.length; i++) {
+                    writer.setUint8(offset, str.charCodeAt(i));
                     offset += 1;
                 }
-                view.setUint8(offset, 0);
+                writer.setUint8(offset, 0);
                 offset += 1;
-            });
-
-            if (debug) {
-                console.log(
-                    `=============== Write Debug ===============`
-                )
-                console.log(
-                    `Magic: ${this.magic}\n` +
-                    `Record count: ${this.record_count}\n` +
-                    `Field count: ${this.field_count}\n` +
-                    `Record size: ${this.record_size}\n` +
-                    `String block size: ${this.string_block_size}\n`
-
-                );
-                console.log(`String block:`);
-                console.log(this.string_block);
             }
 
-            return buffer;
-
+            return new Blob([writer.buffer], {type : 'application/octet-stream'});
         })
     }
 }
 
+async function loadFrom(file)
+{
+    if (typeof file === 'string') {
+        return await fetch(file).then(response => response.arrayBuffer());
+    } else {
+        return await new Promise((resolve, reject) => {
+            var reader = new FileReader();
+            reader.onload = event => resolve(event.target.result);
+            reader.onerror = error => reject(error);
+            reader.readAsArrayBuffer(file);
+        });
+    }
+}
+async function createFrom(file, options)
+{
+    if (typeof file === 'object') {
+        return file;
+    }
+    if (typeof file === 'string') {
+        var text = await fetch(file).then(response => response.text());
+    }
+    if (file instanceof Blob) {
+        var text = await new Promise((resolve, reject) => {
+            var reader = new FileReader();
+            reader.onload = event => resolve(event.target.result);
+            reader.onerror = error => reject(error);
+            reader.readAsText(file);
+        });
+    }
 
+    var lines = text.split(options.lineBreak);
+    if (options.header) var header = lines.shift().split(options.delimiter);
 
+    var rows = lines.map(line => {
+        var fields = line.split(options.delimiter);
+        var obj = {};
+        for (var i = 0; i < fields.length; i++) {
+            obj[header[i]] = fields[i];
+        }
+        return obj;
+    });
+
+    return rows;
+}
