046c0229f6ed7bcd610407ea2ed8dd3f6569c707
Trimitor <p.trimitor@gmail.com>
2024-02-12 22:03:18 +0300

    Update worker-read.js

--- tools/js/worker-read.js
+++ tools/js/worker-read.js
@@ -3,6 +3,6 @@ importScripts("DBCUtil.js");
 onmessage = async (e) => {
     const { path, schema } = e.data;
     const obj = new DBC(`../${path}`, `../${schema}`);
-    const result = await obj.read();
+    const result = await obj.read({type : 'array', delimiter : ',', lineBreak : '\r\n'});
     postMessage(result);
 }
