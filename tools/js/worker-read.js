e69d8c301b2a3c974a945af09ea025c34e65bfab
Trimitor <73941033+Trimitor@users.noreply.github.com>
2026-06-05 09:23:29 +0300

    clean project

--- tools/js/worker-read.js
+++
@@ -1,8 +0,0 @@
-importScripts("DBCUtil.js");
-
-onmessage = async (e) => {
-    const { path, schema } = e.data;
-    const obj = new DBC(`../${path}`, `../${schema}`);
-    const result = await obj.read({type : 'array', delimiter : ',', lineBreak : '\r\n'});
-    postMessage(result);
-}
--- tools/js/worker-write.js
+++
@@ -1,8 +0,0 @@
-importScripts("DBCUtil.js");
-
-onmessage = async (e) => {
-    const { path, schema } = e.data;
-    const wdbc = new DBC(path, `../${schema}`);
-    const result = await wdbc.write();
-    postMessage(result);
-}
