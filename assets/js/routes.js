d3553eaac2c4e3cd645fc8fa800d22e8b495582d
Trimitor <73941033+Trimitor@users.noreply.github.com>
2026-06-05 10:48:42 +0300

    Update routes.js

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -1,8 +1,4 @@
 export const routes = [
   { path: '/', view: 'home' },
-  { path: '/patches', view: 'patches' },
-  { path: '/compatches', view: 'compatches' },
-  { path: '/compatch', view: 'compatch' },
   { path: '/download', view: 'download' },
-  { path: '/pb', view: 'pb' },
 ];
